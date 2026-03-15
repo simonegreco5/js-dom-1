d0ea8e63b668c807904b359cdd38bafba428ec04
simonegreco5 <simoxey.greco@gmail.com>
2026-03-15 11:41:58 +0100

    add function in js and fixs

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 console.log('js-dom');
 
 // selezioniamo il nodo dom (in questo caso il button)
-const buttonScript = document.querySelector('button');  
+const buttonScript = document.querySelector('button');
+
 // scriviamo la funzione usando il metodo Event Listeners
 buttonScript.addEventListener('click', function(){
 
-    
+    // selezioniamo il noso dom (img) (con getElementById perché sfruttiamo l'id)
+    const imgScript = document.getElementById('lamp')
+
+    imgScript.src = './img/yellow_lamp.png'
+    console.log('you clicked switch, now lamp is on')
+
 })
