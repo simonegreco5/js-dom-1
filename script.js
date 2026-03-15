71948733064bab6e164b9b0992cfc54de4a15a32
simonegreco5 <simoxey.greco@gmail.com>
2026-03-15 11:34:01 +0100

    add node dom (button) and create a function with event listeners

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('js-dom');
+
+// selezioniamo il nodo dom (in questo caso il button)
+const buttonScript = document.querySelector('button');  
+// scriviamo la funzione usando il metodo Event Listeners
+buttonScript.addEventListener('click', function(){
+
+    
+})
