452a754ad24ce8cabac13ea35ce9583e1e49df0b
simonegreco5 <simoxey.greco@gmail.com>
2026-03-15 23:51:15 +0100

    add fix

--- script.js
+++ script.js
@@ -8,11 +8,15 @@ const imgScript = document.getElementById('lamp')
 
 // scriviamo la funzione usando il metodo Event Listeners
 // con questa funzione, quando clicchiamo il bottone
-// la lampada deve accendersi (cambiando src nell'img)
+// la lampada deve accendersi (cambiando il link src dell'img)
 buttonScript.addEventListener('click', function(){
 
     imgScript.src = './img/yellow_lamp.png'
+    imgScript.alt = 'lamp_on'
+
     console.log('you clicked switch, now lamp is on')
+
+   
 
 })
 
