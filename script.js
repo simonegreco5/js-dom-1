94104a476948f56ec099b04be0975b3508d7b8c6
simonegreco5 <simoxey.greco@gmail.com>
2026-03-16 20:30:27 +0100

    add property (innerHTML) for button to switch its string (accendi / spegni)

--- script.js
+++ script.js
@@ -11,11 +11,31 @@ const imgScript = document.getElementById('lamp')
 // la lampada deve accendersi (cambiando il link src dell'img)
 buttonScript.addEventListener('click', function(){
 
-    imgScript.src = './img/yellow_lamp.png'
-    imgScript.alt = 'lamp_on'
+    // imgScript.src = './img/yellow_lamp.png'
+    // imgScript.alt = 'lamp_on'
 
-    console.log('you clicked switch, now lamp is on')
 
+   console.log(imgScript.src.includes('yellow'))
+   
+   if (imgScript.src.includes('yellow')){
+        // lampada spenta
+
+        imgScript.src = './img/white_lamp.png'
+        console.log('you clicked switch, now lamp is off')
+        buttonScript.innerHTML = 'ACCENDI'
+        // buttonScript.classList.add('')
+
+    } else { 
+        // lampada accesa
+
+        console.log(buttonScript.classList);
+
+        imgScript.src = './img/yellow_lamp.png'
+        console.log('you clicked switch, now lamp is on')
+        buttonScript.innerHTML = 'SPEGNI'
+        // buttonScript.classList.remove('')
+
+    } 
    
 
 })
